ab5e8905929c775d8de0eafd766a0c8097e6be23
Mesnyankin Vladislav <goosts@mail.ru>
2021-12-18 10:23:35 +0300

    Решение блока "Работа с данными"

--- src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleRegExpService.java
@@ -1,5 +1,11 @@
 package com.epam.izh.rd.online.service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleRegExpService implements RegExpService {
 
     /**
@@ -11,7 +17,19 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String maskSensitiveData() {
-        return null;
+        String string = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/sensitive_data.txt"))) {
+            string = bufferedReader.readLine();
+            Pattern pattern = Pattern.compile("\\d{4} \\d{4} \\d{4} \\d{4}");
+            Matcher matcher = pattern.matcher(string);
+            while (matcher.find()) {
+                String newString = matcher.group().replaceAll(" \\d{4} \\d{4}", " **** ****");
+                string = string.replace(matcher.group(), newString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return string;
     }
 
     /**
@@ -22,6 +40,14 @@ public class SimpleRegExpService implements RegExpService {
      */
     @Override
     public String replacePlaceholders(double paymentAmount, double balance) {
-        return null;
+        String string = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/sensitive_data.txt"))) {
+            string = bufferedReader.readLine();
+            string = string.replaceAll("\\$\\{payment_amount}", String.valueOf((int) paymentAmount));
+            string = string.replaceAll("\\$\\{balance}", String.valueOf((int) balance));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return string;
     }
 }
